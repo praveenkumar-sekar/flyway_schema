bdab40b193a3fdcf4e55fe68450fd582578c13cf
praveenkumar-sekar <93420852+praveenkumar-sekar@users.noreply.github.com>
2021-12-02 11:32:47 +0530

    Add files via upload

---
+++ spring_flyway/src/main/resources/db/migration/V4_insertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO REGISTRATION_USERS(username,first_name,last_name,email,mobile)('praveenkumar','praveen','kumar','praveenduke2000@gamil.com','7358439739');
+INSERT INTO REGISTRATION_USERS(username,first_name,last_name,email,mobile)('sureshraina','suresh','raina','raian@gmail.com','6768789787');
